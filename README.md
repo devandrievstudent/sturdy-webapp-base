2c01d343c4dd548292fd41660764128698a36828
devandriev <devandriev@gmail.com>
2023-05-26 20:44:35 +0300

    v0.1 Set up rollup.config.js, index.js, bundle.js and add deps to index.html.

---
+++ README.md
@@ -0,0 +1,9 @@
+# ⚡ Sturdy web app base
+
+## Initial setup
+This repository comes without the `node_modules` folder, so before using this library and/or building please run:  ```npm install``` to install the required modules.
+## Usage
+The web app base uses `@material/web` library, so to use the Material Design components follow the guide at the [Material Web's repository.](https://github.com/material-components/material-web/tree/main/docs/components)
+
+## Build
+Once you are done writing your code, you can build the project to html by using `rollup -c`. If you do not allow external PowerShell scripts, you can use `npx rollup -c` instead.
--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
 // Import rollup plugins
-import html from '@web/rollup-plugin-html';
+import {rollupPluginHTML as html} from '@web/rollup-plugin-html';
 import {copy} from '@web/rollup-plugin-copy';
 import resolve from '@rollup/plugin-node-resolve';
-import {terser} from '@rollup/plugin-terser';
-import minifyHTML from 'rollup-plugin-minify-html-literals';
+import terser from '@rollup/plugin-terser';
+import pkgMinifyHTML from 'rollup-plugin-minify-html-literals';
+const minifyHTML = pkgMinifyHTML.default;
 import summary from 'rollup-plugin-summary';
 
 export default {
@@ -12,6 +13,25 @@ export default {
         html({
             input: "index.html"
         }),
-
-    ]
+        // Розв'язати імена модулів
+        resolve(),
+        // Minify HTML template literals
+        minifyHTML(),
+        // Minify JS
+        terser({
+            ecma: 2020,
+            module: true,
+            warnings: true,
+        }),
+        // Print bundle summary
+        summary(),
+        // Optional: copy any static assets to build directory
+        // copy({
+        //     patterns: ['images/**/*'],
+        // })
+    ],
+    output: {
+        dir: 'build'
+    },
+    preserveEntrySignatures: "strict"
 }
